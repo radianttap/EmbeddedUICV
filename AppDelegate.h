2a5d4bda779fe3e1e2189cb9431fc74038f839bd
Aleksandar Vacic <av@codeaplus.com>
2015-06-22 14:44:25 +0200

    Initial commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmbeddedUICVTest
+//
+//  Created by Aleksandar Vacić on 22.6.15..
+//  Copyright © 2015. Radiant Tap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Units/RTUnitLayoutAttributes.m
@@ -0,0 +1,47 @@
+//
+//  RTUnitLayoutAttributes.m
+//  Convert All Units
+//
+//  Created by Aleksandar Vacić on 1.10.13..
+//  Copyright (c) 2013. Radiant Tap. All rights reserved.
+//
+
+#import "RTUnitLayoutAttributes.h"
+
+@implementation RTUnitLayoutAttributes
+
+- (id)copyWithZone:(NSZone *)zone {
+	
+	RTUnitLayoutAttributes *attributes = [super copyWithZone:zone];
+	attributes.screenViewCenterOffset = self.screenViewCenterOffset;
+	attributes.numberValueSwitched = self.numberValueSwitched;
+	attributes.isSource = self.isSource;
+	attributes.leftAligned = self.leftAligned;
+	
+	return attributes;
+}
+
+- (BOOL)isEqual:(id)other {
+	if (other == self) {
+		return YES;
+	}
+	if (!other || ![[other class] isEqual:[self class]]) {
+		return NO;
+	}
+	if ([((RTUnitLayoutAttributes *) other) screenViewCenterOffset] != [self screenViewCenterOffset]) {
+		return NO;
+	}
+	if ([((RTUnitLayoutAttributes *) other) numberValueSwitched] != [self numberValueSwitched]) {
+		return NO;
+	}
+	if ([((RTUnitLayoutAttributes *) other) isSource] != [self isSource]) {
+		return NO;
+	}
+	if ([((RTUnitLayoutAttributes *) other) leftAligned] != [self leftAligned]) {
+		return NO;
+	}
+	
+	return YES;
+}
+
+@end
